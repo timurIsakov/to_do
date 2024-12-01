ad3a1269a66b7a58d9734ccd50eb4a7401113a99
timurIsakov <timur092000@gmail.com>
2024-12-01 22:15:21 +0500

    update .gitignore

--- app/routers/task.py
+++ app/routers/task.py
@@ -1,13 +1,13 @@
 from typing import Annotated
 
-from fastapi import APIRouter
+from fastapi import APIRouter, Depends
 from fastapi import Body
 from sqlalchemy import insert, select, update, delete
 from sqlalchemy.orm import Session
 from starlette import status
 from starlette.responses import Response
 
-from app.data_source.config import engine
+from app.data_source.config import engine, get_db_session
 from app.data_source.models import Task
 from app.schemas.task import TaskBaseSchemas, TaskReadSchemas
 
@@ -15,10 +15,10 @@ task_router = APIRouter(prefix="/tasks", tags=["tasks"])
 
 
 @task_router.post("/", status_code=status.HTTP_201_CREATED)
-def create_task(user_id: Annotated[int, Body()], task: TaskBaseSchemas):
-    with Session(bind=engine) as session:
-        session.execute(insert(Task).values(**task.model_dump(), user_id=user_id))
-        session.commit()
+def create_task(user_id: Annotated[int, Body()], task: TaskBaseSchemas,
+                session: Annotated[Session, Depends(get_db_session)]):
+    session.execute(insert(Task).values(**task.model_dump(), user_id=user_id))
+    session.commit()
 
 
 @task_router.get("/", response_model=list[TaskReadSchemas])
